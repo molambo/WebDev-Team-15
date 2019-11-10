f74f008fc334ddf4b343ee2e5a55aee83e19f16e
Amanda Escaba <amandaescaba@gmail.com>
2019-11-10 16:13:00 -0500

    added prototype code from class github and inserted our API key and search parameter placeholders

--- src/containers/PluggedInPrototype.js
+++ src/containers/PluggedInPrototype.js
@@ -2,14 +2,66 @@ import React from 'react';
 import {faPlug} from "../../node_modules/@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '../../node_modules/@fortawesome/react-fontawesome';
 import './PluggedInPrototype.css'
+import './../config'
 
 export class PluggedInPrototype extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            videos: [],
+            video: {
+                title: '',
+                description: ''
+            }
         }
-
     }
+    componentDidMount() {
+        console.log("Component Did Mount")
+        // this.findMovieByTitle("batman")
+    }
+
+    // createReview = review =>
+    //     fetch("http://localhost:8080/api/reviews", {
+    //         method: 'post',
+    //         headers: {
+    //             'content-type': 'application/json'
+    //         },
+    //         body: JSON.stringify(review)
+    //     }).then(response => response.json())
+    //         .then(review => console.log(review))
+
+    searchVideo = searchTitleChanged =>
+        this.findVideoByTitle(searchTitleChanged)
+
+    findVideoByTitle = title =>
+        fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${maxresults}}&q=${searchphrase}&type=video&key=&AIzaSyDzAog-ZQbUDu3qB2I0JCoyleKhfguoVNs`)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response.Search)
+                this.setState({
+                    videos: response.Search
+                })
+            })
+
+    selectVideo = imdbID =>
+        fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${maxresults}}&q=${searchphrase}&type=video&key=&AIzaSyDzAog-ZQbUDu3qB2I0JCoyleKhfguoVNs`)
+            .then(response => response.json())
+            .then(video => {
+                console.log(video)
+                this.setState({
+                    video: video
+                })
+
+                // fetch(`http://localhost:8080/api/review/${imdbID}`)
+                //     .then(response => response.json())
+                //     .then(review => {
+                //         console.log(review)
+                //         this.setState({
+                //             review: review
+                //         })
+                //     })
+
+            })
 
     render() {
         return(
